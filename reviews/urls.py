7f425d30f2b79b80dc04ec773e7466520d985b32
Jan Skoda <skoda@jskoda.cz>
2019-02-11 11:58:51 +0100

    Support adding new reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name = 'index'),
+    path('new_review', views.new_review, name = 'new_review'),
+    path('submit_review', views.submit_review, name = 'submit_review'),
+    path('about', views.about, name = 'about'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,14 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse
 
-from .models import Review
+from .models import Review, Product
 
 
 
 def index(request):
     latest_reviews = Review.objects.order_by('-creation_time')[:5]
     context = {
-            'reviews': latest_reviews
+            'reviews': latest_reviews,
     }
     return render(request, 'index.html', context)
-    # return HttpResponse("Hello world!")
+
+
+def new_review(request):
+    insurances = Product.objects.all()
+    context = {
+        'insurances': insurances,
+    }
+    return render(request, 'new_review.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def submit_review(request):
+    try:
+        selected_product = Product.objects.get(name = request.POST['insurance'])
+    except KeyError:
+        selected_product = Product(name = request.POST['insurance'])
+        selected_product.save()
+    review = Review(
+        author = request.POST['name'],
+        author_ip = request.META.get('REMOTE_ADDR'),
+        product = selected_product,
+        text = request.POST['message'],
+        rating = 0, # TODO
+        creation_time = timezone.now(),
+    )
+    review.save()
+    return HttpResponseRedirect(reverse('index'))
+
